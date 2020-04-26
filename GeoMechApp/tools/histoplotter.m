1364adec4313b3da87a22ed407add9be0031e3d4
Satyam Pratap Singh <39654260+singhsatyampratap@users.noreply.github.com>
2020-04-26 21:04:25 +0530

    Update and rename GeoMechApp/histoplotter.m to GeoMechApp/tools/histoplotter.m

--- GeoMechApp/tools/histoplotter.m
+++ GeoMechApp/tools/histoplotter.m
@@ -1,4 +1,13 @@
 function histoplotter(data,bin,xlab,plot_title)
+    %This function plot the histogram of the data with specific formatting
+    
+    %input parameter
+    %data:data vector
+    %bin: no of the bins of histogram(int)
+    %xlab:xaxis label(string)
+    %plot_title:Depth at which the discrete data distribution is to be shown as " Distribution of xlab at Depth(plot_title)"
+       
+    
     histogram(data,bin)
     m=string(mean(data));
     sd=string(std(data));
