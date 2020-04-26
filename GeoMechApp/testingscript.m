9d74b840162e48f76c711157a2b8ea78e5bcbc4a
Satyam Pratap Singh <39654260+singhsatyampratap@users.noreply.github.com>
2020-04-27 04:59:54 +0530

    Update testingscript.m

--- GeoMechApp/testingscript.m
+++ GeoMechApp/testingscript.m
@@ -18,3 +18,44 @@ hold on
 plot(sigma_v,phi)
 figure
 histoplotter(phin,15,"Porosity","1000-2000 m")
+%%
+%test load data
+clear all
+close 
+clc
+log=data_loader('/MATLAB Drive/GeoMechSAT/GeoMech/GOMDATA.xlsx')
+%%
+%test Sv_calculator
+log.Sv=Sv_calculator(log.Density,log.Depth,2700,9.8,'a')
+plot(log.Sv,-log.Depth)
+%%
+%test elastic_parameter
+[log.E,log.G,log.M,log.v]=elastic_parameter(log.VP,log.VS,log.Density);
+subplot(1,4,1)
+plot(log.E,-log.Depth)
+subplot(1,4,2)
+plot(log.G,-log.Depth)
+subplot(1,4,3)
+plot(log.M,-log.Depth)
+subplot(1,4,4)
+plot(log.v,-log.Depth)
+
+%%
+%PorePressure calculation
+log.PP=PorePressure_calculator(log.Depth,1100,9.8,'hydrostatic')
+plot(log.PP,-log.Depth)
+
+%%
+%Shmin Calculation
+log.Shmin=Shmin_calculator(log.Sv,log.PP,log.v,'movmean',100)
+plot(log.Shmin,-log.Depth)
+
+%%
+% plot all stress and pore pressure
+figure
+hold on
+plot(log.Sv,-log.Depth)
+plot(log.Shmin,-log.Depth)
+plot(log.PP,-log.Depth)
+legend 'Overburden' 'Minimum Horizontal' 'Pore Pressure' 
+
